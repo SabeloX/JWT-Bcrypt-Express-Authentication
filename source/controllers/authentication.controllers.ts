09a3b60701bfffd7ebead5c5993ee3eec1ed590b
Sabelo <sabelomtw@gmail.com>
2021-10-20 20:00:43 +0200

    Login controller

--- source/controllers/authentication.controllers.ts
+++ source/controllers/authentication.controllers.ts
@@ -15,16 +15,40 @@ export const register = (request: Request, response: Response, next: Function) =
         connectDB( async (database: Db) => {
 
             //check if user exists in the database
-            const user = await database.collection('users').findOne({ username: request.body.username });
+            const user = await database.collection('users').findOne({ username });
 
             if(!user){
-                const newUser = await database.collection('users').insertOne({
-                    username: request.body.username,
-                    password: request.body.password
-                });
+                const newUser = await database.collection('users').insertOne({ username, password });
                 response.status(200).send(newUser);
             }
             response.status(403).send({ message: `User already exists` });
+        }, response);
+    }
+    response.status(400).send({ message: `Cannot submit a blank username/password` });
+}
+
+/**
+ * Login a user
+ * @param request 
+ * @param response 
+ * @param next 
+ */
+export const login = (request: Request, response: Response, next: Function) => {
+    const {username, password} = request.body;
+    if(username && password){
+        connectDB( async (database: Db) => {
+            //check if user exists in the database
+            const user = await database.collection('users').findOne({ username });
+            if(!user){
+                response.status(401).send({ message: `Incorrect username/password` });
+            }
+            else{
+                if(user.password === password){
+                    response.status(200).send(user);
+                }
+                response.status(401).send({ message: `Incorrect username/password`});
+            }
+
         }, response);
     }
     response.status(400).send({ message: `Cannot submit a blank username/password` });
