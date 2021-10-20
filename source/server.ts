672fce046a7bef31c3a669b1526996a696854427
Sabelo <sabelomtw@gmail.com>
2021-10-20 19:09:28 +0200

    Removed database connection from server

--- source/server.ts
+++ source/server.ts
@@ -1,25 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import { MongoClient } from 'mongodb';
 import router from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const databaseURL = process.env.DATABASE || 'mongodb://localhost:27017';
 
 app.use(json());    // allow server to read JSON format data
 app.use(cors());    // enable CORS for all requests
 
 // Routes
 app.use('/api/users', router.auth);
-
-// Connect to the database
-MongoClient.connect(databaseURL, (error, database) => {
-    if(error){
-        console.log(`Cannot connect to the database: \n${error.message}`);
-    }
-    console.log(`Successfully connected to the database.`);
-});
 
 // listen for the server on PORT
 app.listen(PORT, () => console.log(`Server Running in http://localhost:${PORT}`));
