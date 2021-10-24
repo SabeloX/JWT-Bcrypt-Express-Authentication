bc4a6c385c270cc15e8c2c81f8ef47e887035bf4
Sabelo <sabelomtw@gmail.com>
2021-10-24 11:37:54 +0200

    sUser routes

--- source/routes/index.ts
+++ source/routes/index.ts
@@ -2,6 +2,7 @@
  * Export all routes here
  */
 
-import auth from './authentication.routes';
+import auth from './users/authentication.routes';
+import users from './users/user.routes';
 
-export default { auth };
+export default { auth, users };
