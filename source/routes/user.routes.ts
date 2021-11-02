ec560611c8c4cf5c5050ea569dbcef6342f5e5ce
Sabelo <sabelomtw@gmail.com>
2021-11-02 22:07:36 +0200

    Added middleware to user routes

--- source/routes/user.routes.ts
+++ source/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import controllers from '../controllers';
+import middleware from "../middleware";
 
 const router = Router();
 
 router
-    .get('/', controllers.getUsers)
-    .get('/:username', controllers.getUser)
+    .get('/', middleware.verifyToken, controllers.getUsers)
+    .get('/:username', middleware.verifyToken, controllers.getUser)
 
 export default router;
