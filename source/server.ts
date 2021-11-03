506ab9c2b4b0c01a3690f89769f5d884e3c971c5
Sabelo <sabelomtw@gmail.com>
2021-11-03 12:58:23 +0200

    Added urlencoded to true

--- source/server.ts
+++ source/server.ts
@@ -1,4 +1,4 @@
-import express, { json } from 'express';
+import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 import router from './routes';
 import mongoose from 'mongoose';
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const databaseURL: string = process.env.DATABASE || '';
 
+app.use(urlencoded({ extended: true }));
 app.use(json());    // allow server to read JSON format data
 app.use(cors());    // enable CORS for all requests
 
